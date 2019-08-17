6c18bf250df7fc23fb6b73b2814c907a241bf675
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-08-17 17:49:07 -0400

    registration form validation

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app.config['SECRET_KEY'] = '061a9d20aa9aec2f897c7e0e115f65c0'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 # no format
 from flaskblog import routes
