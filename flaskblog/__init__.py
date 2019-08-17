c4fd050d596a3b0cd16765b1cc30362f61468e69
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-08-17 18:28:33 -0400

    redirect

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,7 +11,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+login_manager = LoginManager(app) #powerful login library
+login_manager.login_view = 'login' # route function name
+login_manager.login_message_category = 'info' #style alert message
 
 # no format
 from flaskblog import routes
