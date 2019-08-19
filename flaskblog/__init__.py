815404bb60457538c32fc6370501d9f236705903
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-08-18 20:18:42 -0400

    reset password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -16,6 +18,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app) #powerful login library
 login_manager.login_view = 'login' # route function name
 login_manager.login_message_category = 'info' #style alert message
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+mail = Mail(app)
 
 # no format
 from flaskblog import routes
